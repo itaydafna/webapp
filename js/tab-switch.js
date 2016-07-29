1ad11d061ce2e98efc6f90dfca66188e4bdac03e
itaydafna <itaydafna@gmail.com>
2016-07-29 22:02:24 +0300

    tabs switch working with JS script

--- js/tab-switch.js
+++ js/tab-switch.js
@@ -1,15 +1,21 @@
+//-----------------------------------
 //this script controls the tab switch
+//-----------------------------------
 
 (function () {
 
+//-------------------------------
+//dealing with tab style switching
+//-------------------------------
+    
     // creating a node-list of all the tabs
     var tabsList = document.querySelectorAll(".tabs li"),
     // creating an array of all the tabs
         tabsArr = Array.prototype.slice.call(tabsList);
 
-    //function which adds an "active" class to a clicked tab and removes this class from all rest of the tabs
+    //function which adds an "active-tab" class to a clicked tab and removes this class from all rest of the tabs
 
-    function setActive(){
+    function setActiveTab(){
         var clickedTab = this;
         if(clickedTab.className !== "active-tab"){
             tabsArr.forEach(function(tab){
@@ -23,11 +29,44 @@
         }
 
     }
+    
+//-------------------------------------------
+//dealing with active content div switching
+//------------------------------------------
 
-    //function which adds "click" event listeners to all tabs for changing to active state
+    // creating a node-list of all the main content divs
+    var divsList = document.querySelectorAll(".content-div"),
+    // creating an array of all the main content tabs
+        divsArr = Array.prototype.slice.call(divsList);
+
+    //function which adds an "active-div" class to a corresponding clicked tab and removes this class from all rest of the tabs
+
+    function setActiveDiv(){
+        var clickedTab = this;
+        //finding the index of the clicked tab in the tabs array
+        var clickedTabIndex = tabsArr.indexOf(clickedTab);
+        for(var i = 0; i< divsArr.length; i++){
+            if(i===clickedTabIndex){
+                divsArr[i].dataset.active = "active";
+            } else{
+                divsArr[i].dataset.active = "inactive";
+            }
+        }
+    }
+
+//----------------------
+//adding event listeners
+//----------------------
+
+//function which adds "click" event listeners to all tabs for changing to active state
 
     (function appendSetActive(){
-        tabsArr.forEach(function(tab){tab.addEventListener("click",setActive);
-    })}());
+        tabsArr.forEach(
+            function(tab){
+                tab.addEventListener("click",setActiveTab);
+                tab.addEventListener("click",setActiveDiv);
+            })}());
+
 
 }())
+
